2c72f215aeb24646e1c2b1609f78732d4ff73af1
Rafael Montenegro <rafael@mesainc.com.br>
2019-09-02 15:34:23 -0700

    Switch tag to load first component matched in route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
 
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -21,15 +21,20 @@ class App extends Component {
 
           <Navbar />
 
-          {/* The Route component shows the Component based on the path you specified */}
+          {/* Will make just the first matched Route be loaded! */}
+          <Switch>
 
-          {/* We use "exact" so React wont load HOME+ABOUT or HOME+CONTACT. */}
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
+            {/* The Route component shows the Component based on the path you specified */}
 
-          {/* Route parameter: */}
-          <Route exact path='/:post_id' component={Post} />
+            {/* We use "exact" so React wont load HOME+ABOUT or HOME+CONTACT. */}
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+
+            {/* Route parameter: */}
+            <Route exact path='/:post_id' component={Post} />
+          
+          </Switch>
 
         </div>
 
